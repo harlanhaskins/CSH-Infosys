5eef319bd294e5cd65b73e7942c0fff6d038afee
Harlan Haskins <harlan@harlanhaskins.com>
2014-05-20 02:07:13 -0400

    Added Reddit module.

---
+++ reddit.py
@@ -0,0 +1,53 @@
+import requests
+import xmlrpclib
+import re
+import argparse
+
+parser = argparse.ArgumentParser(description='Read from a subreddit.')
+parser.add_argument("-r", "--subreddit", help="specify the subreddit from which to read.")
+
+args = parser.parse_args()
+
+subreddit = "/r/" + args.subreddit if args.subreddit else "/hot"
+print subreddit
+
+hotPosts = requests.get('http://www.reddit.com' + subreddit + '.json')
+
+if subreddit == "/hot":
+    subreddit = "Reddit"
+print subreddit
+
+posts = hotPosts.json()["data"]["children"]
+
+title = ""
+for post in posts:
+    if not post["data"]["stickied"]:
+        title = post['data']['title']
+        break
+
+REDDIT_HEADER = "33"
+REDDIT_FILE = "34"
+
+server = xmlrpclib.ServerProxy("http://infosys.csh.rit.edu:8080")
+
+flash = False
+
+if not server.fileExists(REDDIT_HEADER):
+    server.delFile(REDDIT_HEADER)
+    server.addFile(REDDIT_HEADER)
+    flash = True
+
+    server.addText(REDDIT_HEADER, "ROTATE", "Top " + subreddit + " Post:")
+    server.addText(REDDIT_HEADER, "ROTATE", " %" + REDDIT_FILE, REDDIT_FILE)
+
+if not server.fileExists(REDDIT_FILE):
+    server.delFile(REDDIT_FILE)
+    server.addFile(REDDIT_FILE)
+    flash = True
+
+server.addString(REDDIT_FILE, title)
+
+flash = bool(title) or flash
+
+if flash:
+    server.updateSign()
