3dfbf7ff3e8eba2f09f43c11a8de7a38d94b6577
Harlan Haskins <harlan@harlanhaskins.com>
2014-05-20 04:04:00 -0400

    Added queueing to Reddit script.

--- reddit.py
+++ reddit.py
@@ -29,44 +29,67 @@ def normalizedTitle(title):
     title = re.sub(r'[\[\(]([fmFM])[\]\)]', '\\1', title)
     return title
 
-def postTitleToSign(title):
-    REDDIT_HEADER = "33"
-    REDDIT_FILE = "34"
+def postTitleToSign(title, subreddit, exclude_leadin=False, REDDIT_HEADER="33", REDDIT_FILE="34"):
+    if not title:
+        return
 
     server = xmlrpclib.ServerProxy("http://infosys.csh.rit.edu:8080")
 
-    flash = False
-
-    if not server.fileExists(REDDIT_HEADER):
+    if server.fileExists(REDDIT_HEADER):
         server.delFile(REDDIT_HEADER)
-        server.addFile(REDDIT_HEADER)
-        flash = True
 
-        server.addText(REDDIT_HEADER, "ROTATE", "%" + REDDIT_FILE, REDDIT_FILE)
-
-    if not server.fileExists(REDDIT_FILE):
+    if server.fileExists(REDDIT_FILE):
         server.delFile(REDDIT_FILE)
-        server.addFile(REDDIT_FILE)
-        flash = True
 
-    server.addString(REDDIT_FILE, title)
+    server.addFile(REDDIT_HEADER)
+    server.addFile(REDDIT_FILE)
 
-    flash = bool(title) or flash
+    if subreddit == "/hot":
+        subreddit = "Reddit"
 
-    if flash:
-        server.updateSign()
+    if not exclude_leadin:
+        leadin = "Top post from " + subreddit + ":"
+        server.addText(REDDIT_HEADER, "HOLD", leadin, REDDIT_HEADER)
+    server.addText(REDDIT_FILE, "ROTATE", title, REDDIT_FILE)
+
+    server.updateSign()
+
+def files(fileID):
+    secondID = fileID + 1
+    return (str(fileID), str(secondID))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Read from a subreddit.')
     parser.add_argument("-r", "--subreddit", help="specify the subreddit from which to read.")
-    parser.add_argument("-l", "--exclude-lead", 
+    parser.add_argument("-l", "--exclude-leadin", 
                         help="Specifies that this subreddit will not be identified prior to display.", action="store_true")
+    parser.add_argument("-f", "--fileID", help="The file number you'd like (30-40)")
 
     args = parser.parse_args()
+
+    if args.fileID:
+        fileID = args.fileID
+    else:
+        fileID = "33"
+
+    INVALID_FILE_ERROR = "File must be in range 30 - 39"
+
+    try:
+        fileInt = int(fileID)
+    except ValueError:
+        print(INVALID_FILE_ERROR)
+
+    if not fileInt in range(30, 38):
+        print(INVALID_FILE_ERROR)
+        exit(0)
+
+    headerID, fileID = files(fileInt)
 
     subreddit = normalizedSubreddit(args.subreddit)
     title = topTitle(subreddit)
     if not title:
+        print("Title not found.")
         exit(0)
-    postTitleToSign(title)
+
+    postTitleToSign(title, subreddit, exclude_leadin=args.exclude_leadin, REDDIT_HEADER=headerID, REDDIT_FILE=fileID)
 
